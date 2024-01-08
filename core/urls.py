f5c4de37b57802da60fb0ddffe8eeb40c674660d
semper44 <emmanuelcopeters@gmail.com>
2024-01-08 11:36:45 +0000

    copied my old cold to this

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('chat/', include('chat.urls')),
+    path('', include('hometweet.urls')),
+    path('', include('profiles.urls')),
+    path('', include('chat.urls')),
+    path('', include('groupapp.urls')),
+    # path('compressor/', include('compressor.urls')),
+    # path('', include('groupapp.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
