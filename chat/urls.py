9f23f44dfe9e0b2514f70419d004094e4ed5c065
semper44 <emmanuelcopeters@gmail.com>
2024-01-08 13:27:55 +0000

    add my old code

--- chat/urls.py
+++ chat/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     # path('', views.index, name='index'),
-    path('<str:room_name>/', views.room, name='room'),
+    # path('<str:room_name>/', views.room, name='room'),
      path('', views.home_view, name="home"),
     path('messages/', views.chatIntialMessage, name="chatintialmessage"),
     path('selfmessages/', views.chatIntialMessageJson, name="chatintialmessageJson"),
