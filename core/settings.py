f2838877a19033ce5ac6da2099a2f46f9ae482e7
semper44 <56490260+semper44@users.noreply.github.com>
2024-01-11 12:48:14 +0000

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -158,7 +158,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": [redis://red-cmfanm6n7f5s7384kij0:6379],
         },
     },
 }
