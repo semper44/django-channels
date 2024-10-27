25ed8537bbfa3ac81f1b499b8932b946bef8a8f0
Emmanuel peters <56490260+semper44@users.noreply.github.com>
2024-10-27 12:38:01 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,7 @@ pip install -r requirements.txt
 
 # run Migrations
 python manage.py collectstatic --no-input
+python manage.py migrate --fake user zero
+python manage.py migrate --fake admin zero
+python manage.py migrate --fake-initial
 python manage.py migrate
